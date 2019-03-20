90383ac39432e332f7ff5c5439b98b380b1b7ce4
Brent Smith <MachineAlfr@users.noreply.github.com>
2019-03-19 18:15:04 -0700

    Add files via upload
    Code for transmitter/receiver HyLight-Arduino interface boards.
    
    Any form of serial communication can be used to send data to the a transmitter. The recommended method is PySerial. The baud must be set to 9600 and the data must be a string with 11 digits, e.g. "1 100 500 300 900 0 250 80 600 700 0". This string tells the transmitter to send 10 PWM values to a specific HyLight, in this case HyLight #2 will set LED 1, 2, 3, 4, 5, 7, 8, and 9 to a duty cycle of 100, 500, 300, 900, 250, 80, 600, and 700, respectively. LEDs 5 and 10 are not turned on, i.e. the duty cycle is set to 0. The first digit indicates the HyLight number, so a value of 0 corresponds to HyLight #1, a value of 1 to HyLight #2, and so on. The next ten digits indicates the duty cycle of the 10 HyLight LEDs, with 4096 being the absolute max. The HyLight will never reach the max power of one LED, and will continuously turn off if the duty cycle is set to max. Always stay below 50% of any LEDs max duty cycle to avoid damaging the HyLighter boards.

---
+++ HyLight_NRF_receiver.ino
@@ -0,0 +1,50 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+#define CE_PIN  9
+#define CSN_PIN 10
+
+const byte thisSlaveAddress[5] = {'P','o','4','G','J'};
+/* Other slave addresses are:
+ * {'R','x','L','A','A'}
+ * {'R','x','A','Q','B'}
+ * {'B','t','A','q','B'}
+ * {'I','x','E','b','B'}
+ * {'P','c','A','V','S'}
+ * {'A','h','U','p','k'}
+ * {'R','x','h','U','L'}
+ */
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(); // Create a i2c pwm object to control the PCA9685 drivers on the HyLighter interface boards
+RF24 radio(CE_PIN, CSN_PIN); 
+
+int dataReceived[10]; // The 10 integers sent by the transmitter
+
+void setup() {
+    Serial.begin(9600);                         // Begin serial communication if needed
+    radio.begin();                              // Initialize the NRF24l01 module
+    radio.setDataRate( RF24_2MBPS );            // Set the data rate to 2 MBPS
+    radio.openReadingPipe(1, thisSlaveAddress); // Begin listening for broadcasts of the specified receiver address
+    radio.setPayloadSize(255);                  // Set the packet size to 255 bytes
+    radio.startListening();                     // Start being a receiver  
+    radio.setAutoAck(false);                    // Turn off auto-acknowledge to maximize comm speed
+    pwm.begin();                                // Initialize the PCA9685 PWM driver
+    pwm.setPWMFreq(1600);                       // Set the frequency to 1.6 kHz
+    Wire.setClock(400000);                      // Set I2C clock frequency to 400 kHz
+}
+
+void loop() {
+  /* If the NRF24l01 object receives a packet, the packet is
+   * iterated over and the the LED PWM values are set to the 
+   * elements of the 10 element array packet
+   */
+  if(radio.available()){ 
+    radio.read( &dataReceived, sizeof(dataReceived) );
+    for(int i = 0; i < 10; i++){
+      pwm.setPWM((uint8_t) i, 0, (uint16_t) dataReceived[i]);
+    }
+  }
+}
---
+++ HyLight_NRF_transmitter.ino
@@ -0,0 +1,51 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h> // See https://maniacbug.github.io/RF24/ for more 
+
+#define CE_PIN   9
+#define CSN_PIN 10
+
+RF24 radio(CE_PIN, CSN_PIN); // Create a NRF24l01 object using the specified CE/CSN pins
+
+/*
+ * The 8 receiver addresses are listed below, with each receiver flashed to one from the list
+ */
+const byte slaveAddress[8][5] = {{'R','x','L','A','A'},{'R','x','A','Q','B'},{'B','t','A','q','B'},{'I','x','E','b','B'},{'P','c','A','V','S'},{'A','h','U','p','k'},{'R','x','h','U','L'},{'P','o','4','G','J'}};
+
+int rec=10; // Instantiate the receiver number to 10
+
+void setup() {
+    Serial.begin(9600);  // Begin USB serial communication with a baud rate of 9600
+    radio.begin();       // Initiate the NRF24l01 module
+    radio.setPALevel(RF24_PA_MAX); //Set Power Amplifier level to max
+    radio.setDataRate( RF24_2MBPS ); //Set transmission rate to 2 MBPS
+    radio.setRetries(0,0); // delay, count, off for transmission speed
+    radio.stopListening(); // Go into transmission mode only
+    radio.setPayloadSize(255); // 255 bytes
+    radio.setAutoAck(false); // Auto acknowledge turned off for faster communication, but can be turned on if noise becomes an issue.
+    Serial.setTimeout(11);   // Time out serial transmission after 11 ms if nothing received  
+}
+
+void loop() {
+  int data[10]; // Initializing the duty-cycles of the 10 LEDs
+  /*
+   * If serial data is received, read in the 11 digit string and parse out
+   * the string into respective integers. The first integer is the HyLight-
+   * Arduino interface board, the next 10 are the duty cycles of the 10 LEDs
+   */
+  if (Serial.available() > 0) { 
+    rec = Serial.parseInt();
+    data[0] = Serial.parseInt();
+    data[1] = Serial.parseInt();
+    data[2] = Serial.parseInt();
+    data[3] = Serial.parseInt();
+    data[4] = Serial.parseInt();
+    data[5] = Serial.parseInt();
+    data[6] = Serial.parseInt();
+    data[7] = Serial.parseInt();
+    data[8] = Serial.parseInt();
+    data[9] = Serial.parseInt();
+  }
+  radio.openWritingPipe(slaveAddress[rec]); // Open up a pipe to the designated HyLighter
+  radio.write( &data, sizeof(data) );       // Transmit the 10 integer array to the designated HyLighter board
+}
