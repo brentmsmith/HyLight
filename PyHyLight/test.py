ac5fa16260c7f50c280a7157af523ff900a888ef
Brent Smith <MachineAlfr@users.noreply.github.com>
2019-04-02 15:14:37 -0700

    Update test.py

--- PyHyLight/test.py
+++ PyHyLight/test.py
@@ -11,8 +11,7 @@ import numpy as np
 import time
 
 # Open ard serial port
-ard.port = '/dev/ttyACM0' # Change this to your actual transmitter arduino serial port path
-ard.open()
+tran = serial.Serial('/dev/ttyACM0',9600) # Change this to your actual transmitter arduino serial port path
 notes = ['C','Db','D','Eb','E','F','Gb','G','Ab','A','Bb','B']
 off = np.zeros(10) # Array of zeros to turn off HyLights
 
@@ -21,21 +20,21 @@ serial_wait = .05 #wait time between each send in sec
 
 # Cycle through Mary's colors, all the same color
 for note in notes:
-    [(time.sleep(serial_wait),send(ix,hylrgb(maryrgb[note]),power=10)) for ix in hylighter_ix]
+    [(time.sleep(serial_wait),send(tran,ix,hylrgb(maryrgb[note]),power=10)) for ix in hylighter_ix]
     
     time.sleep(1.5)
     
-    [(time.sleep(serial_wait), send(ix, off)) for ix in hylighter_ix]
+    [(time.sleep(serial_wait), send(tran,ix, off)) for ix in hylighter_ix]
 
 # Cycle through Mary's colors, alternating
 i,j,k,l,m,n = 0,1,2,3,4,5
 for note in notes*2:
-	send(0,hylrgb(maryrgb[notes[i]]),power=10)
-	send(1,hylrgb(maryrgb[notes[j]]),power=10)
-	send(2,hylrgb(maryrgb[notes[k]]),power=10)
-	send(3,hylrgb(maryrgb[notes[l]]),power=10)
-	send(4,hylrgb(maryrgb[notes[m]]),power=10)
-	send(5,hylrgb(maryrgb[notes[n]]),power=10)
+	send(tran,0,hylrgb(maryrgb[notes[i]]),power=10)
+	send(tran,1,hylrgb(maryrgb[notes[j]]),power=10)
+	send(tran,2,hylrgb(maryrgb[notes[k]]),power=10)
+	send(tran,3,hylrgb(maryrgb[notes[l]]),power=10)
+	send(tran,4,hylrgb(maryrgb[notes[m]]),power=10)
+	send(tran,5,hylrgb(maryrgb[notes[n]]),power=10)
 	i,j,k,l,m,n=i+1,j+1,k+1,l+1,m+1,n+1
 	if i>11:
 		i=0
@@ -52,16 +51,16 @@ for note in notes*2:
 	time.sleep(1)
 
 
-[(time.sleep(serial_wait), send(ix, off)) for ix in hylighter_ix]
+[(time.sleep(serial_wait), send(tran,ix, off)) for ix in hylighter_ix]
 
 # Cycle through Mary's colors faster
 for note in notes*3:
-	send(0,hylrgb(maryrgb[notes[i]]),power=10)
-	send(1,hylrgb(maryrgb[notes[j]]),power=10)
-	send(2,hylrgb(maryrgb[notes[k]]),power=10)
-	send(3,hylrgb(maryrgb[notes[l]]),power=10)
-	send(4,hylrgb(maryrgb[notes[m]]),power=10)
-	send(5,hylrgb(maryrgb[notes[n]]),power=10)
+	send(tran,0,hylrgb(maryrgb[notes[i]]),power=10)
+	send(tran,1,hylrgb(maryrgb[notes[j]]),power=10)
+	send(tran,2,hylrgb(maryrgb[notes[k]]),power=10)
+	send(tran,3,hylrgb(maryrgb[notes[l]]),power=10)
+	send(tran,4,hylrgb(maryrgb[notes[m]]),power=10)
+	send(tran,5,hylrgb(maryrgb[notes[n]]),power=10)
 	i,j,k,l,m,n=i+1,j+1,k+1,l+1,m+1,n+1
 	if i>11:
 		i=0
@@ -77,4 +76,4 @@ for note in notes*3:
 		n=0
 	time.sleep(.3)
 
-[(time.sleep(serial_wait), send(ix, off)) for ix in hylighter_ix]
+[(time.sleep(serial_wait), send(tran,ix, off)) for ix in hylighter_ix]
